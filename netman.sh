be0563bc6205d6cdcbbd1186262849dc41052c89
Kalma <150836006+crimsonmoon2301@users.noreply.github.com>
2024-05-22 23:16:37 +0300

    More options, better integrated

--- netman.sh
+++ netman.sh
@@ -4,16 +4,17 @@ clear
 
 function netman() {
 	echo "--- NETWORK MANAGMENT ---" 
-	echo "1. Restart IWD"
-	echo "2. Check IWD status"
-	echo "3. Stop IWD"
-	echo "4. Start IWD"
+	echo "1. Restart IWD (Requires root access!)"
+	echo "2. Check IWD status (Requires root access!)"
+	echo "3. Stop IWD (Requires root access!)"
+	echo "4. Start IWD (Requires root access!)"
 	echo "5. Connect to a network via SSID"
 	echo "6. Connect to a network via BSSID (MAC address)"
 	echo "7. Disconnect from a network"
-	echo "8. Show known networks" 
-	echo "9. Forget a known network" 
-	echo "10. Go back" 
+	echo "8. Show networks" 
+	echo "9. Show known networks"
+	echo "10. Forget a known network" 
+	echo "11. Go back" 
 }
 
 function choice() {
@@ -31,10 +32,12 @@ function choice() {
 		6) echo "work in progress!" ;;
 		7) iwctl station wlan0 disconnect ;;
 		8) iwctl station wlan0 scan && iwctl station wlan0 get-networks ;;
-		9) echo "Write the network name:"
+		9) iwctl known-networks list ;;
+		10) echo "Write the network name:"
 			read net
 			iwctl known-networks $net forget ;;
-		10) exit ;;
+		11) 	clear
+			exit ;;
 		*) echo "Invalid choice" ;;
 	esac
 }
